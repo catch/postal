63c56e3826bbb05e1aed778db141823b8acca59a
Christian Hergert <christian@catch.com>
2012-11-16 20:07:18 -0800

    build: try this one more time.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+package="Postal"
+
+if [ "`uname -s`" = "Darwin" ]; then
+	export LIBTOOL=glibtool
+	export LIBTOOLIZE=glibtoolize
+else
+	export LIBTOOL=libtool
+	export LIBTOOLIZE=libtoolize
+fi
+
+DIE=0
+
+($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile $package."
+	echo "Download the appropriate package for your system,"
+	echo "or get the source from one of the GNU ftp sites"
+	echo "listed in http://www.gnu.org/order/ftp.html"
+	DIE=1
+}
+
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
@@ -13,5 +39,5 @@ autoreconf -v --install || exit $?
 test -n "$NOCONFIGURE" ||
 (
 	"$srcdir/configure" "$@" &&
-	echo "Now type 'make' to compile Postal"
+	echo "Now type 'make' to compile ${package}"
 )
