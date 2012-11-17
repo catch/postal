08eda4623088f7112b1b9fba2003b9a185a01fa7
Christian Hergert <christian@catch.com>
2012-11-16 19:59:46 -0800

    build: try glibtool on OS X.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	# GNU libtool wasn't found as "libtool",
+	# so we check if it's known as "glibtool"
+	(glibtool --version) < /dev/null > /dev/null 2>&1 || {
+		echo
+		echo "You must have libtool installed to compile libvirt."
+		echo "Download the appropriate package for your distribution,"
+		echo "or see http://www.gnu.org/software/libtool";
+		DIE=1
+	}
+	# These are only used if glibtool is what we want
+	export LIBTOOL=glibtool
+	export LIBTOOLIZE=glibtoolize
+}
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
