92f01ae8d4566499087ab73af0813974a3729d72
Christian Hergert <christian@catch.com>
2012-11-29 01:55:05 -0800

    tests: add test fetching devices after creation via HTTP.

--- tests/test-postal-http.c
+++ tests/test-postal-http.c
@@ -126,6 +126,56 @@ test2 (void)
    g_clear_object(&gApplication);
 }
 
+static void
+get_devices2_cb (SoupSession *session,
+                 SoupMessage *message,
+                 gpointer     user_data)
+{
+   gchar *str;
+
+   g_assert(SOUP_IS_SESSION(session));
+   g_assert(SOUP_IS_MESSAGE(message));
+
+   g_assert_cmpint(message->status_code, ==, 200);
+
+   str = g_strdup_printf("[\n"
+                         "  {\n"
+                         "    \"device_token\" : \"%064d\",\n"
+                         "    \"device_type\" : \"c2dm\",\n"
+                         "    \"user\" : \"%s\"\n"
+                         "  }\n"
+                         "]",
+                         gC2dmDeviceId,
+                         gAccount);
+   g_assert_cmpstr(message->response_body->data, ==, str);
+   g_free(str);
+
+   g_application_quit(G_APPLICATION(gApplication));
+}
+
+static void
+test3 (void)
+{
+   SoupSession *session;
+   SoupMessage *message;
+   gchar *url;
+
+   gApplication = application_new(G_STRFUNC);
+
+   session = soup_session_async_new();
+   g_assert(SOUP_IS_SESSION(session));
+
+   url = g_strdup_printf("http://localhost:6616/v1/users/%s/devices", gAccount);
+   message = soup_message_new("GET", url);
+   g_assert(SOUP_IS_MESSAGE(message));
+
+   soup_session_queue_message(session, message, get_devices2_cb, NULL);
+
+   g_application_run(G_APPLICATION(gApplication), 0, NULL);
+
+   g_clear_object(&gApplication);
+}
+
 gint
 main (gint argc,
       gchar *argv[])
@@ -146,6 +196,7 @@ main (gint argc,
 
    g_test_add_func("/PostalHttp/get_devices", test1);
    g_test_add_func("/PostalHttp/add_device", test2);
+   g_test_add_func("/PostalHttp/get_devices2", test3);
 
    return g_test_run();
 }
