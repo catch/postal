c354ae46cbf8c75c6a8c7cdf3de62bef1b788b9b
Christian Hergert <christian@catch.com>
2012-11-29 02:02:40 -0800

    tests: add test to fetch device via HTTP.

--- tests/test-postal-http.c
+++ tests/test-postal-http.c
@@ -179,6 +179,55 @@ test3 (void)
    g_clear_object(&gApplication);
 }
 
+static void
+get_device_cb (SoupSession *session,
+               SoupMessage *message,
+               gpointer     user_data)
+{
+   gchar *str;
+
+   g_assert(SOUP_IS_SESSION(session));
+   g_assert(SOUP_IS_MESSAGE(message));
+
+   g_assert_cmpint(message->status_code, ==, 200);
+
+   str = g_strdup_printf("{\n"
+                         "  \"device_token\" : \"%064d\",\n"
+                         "  \"device_type\" : \"c2dm\",\n"
+                         "  \"user\" : \"%s\"\n"
+                         "}",
+                         gC2dmDeviceId,
+                         gAccount);
+   g_assert_cmpstr(message->response_body->data, ==, str);
+   g_free(str);
+
+   g_application_quit(G_APPLICATION(gApplication));
+}
+
+static void
+test4 (void)
+{
+   SoupSession *session;
+   SoupMessage *message;
+   gchar *url;
+
+   gApplication = application_new(G_STRFUNC);
+
+   session = soup_session_async_new();
+   g_assert(SOUP_IS_SESSION(session));
+
+   url = g_strdup_printf("http://localhost:6616/v1/users/%s/devices/%064d", gAccount, gC2dmDeviceId);
+   message = soup_message_new("GET", url);
+   g_assert(SOUP_IS_MESSAGE(message));
+   g_free(url);
+
+   soup_session_queue_message(session, message, get_device_cb, NULL);
+
+   g_application_run(G_APPLICATION(gApplication), 0, NULL);
+
+   g_clear_object(&gApplication);
+}
+
 gint
 main (gint argc,
       gchar *argv[])
@@ -189,7 +238,7 @@ main (gint argc,
    gKeyFile = g_key_file_new();
    g_key_file_load_from_data(gKeyFile, gConfig, -1, 0, NULL);
 
-   gAccount = g_strdup_printf("%024d", g_random_int());
+   gAccount = g_strdup_printf("%024u", g_random_int());
 
    gC2dmDeviceId = g_random_int();
    gC2dmDevice = g_strdup_printf("{\n"
@@ -200,6 +249,7 @@ main (gint argc,
    g_test_add_func("/PostalHttp/get_devices", test1);
    g_test_add_func("/PostalHttp/add_device", test2);
    g_test_add_func("/PostalHttp/get_devices2", test3);
+   g_test_add_func("/PostalHttp/get_device", test4);
 
    return g_test_run();
 }
